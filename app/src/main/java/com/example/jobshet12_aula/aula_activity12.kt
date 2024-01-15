b98c7479a666c16e3af8107141ed9c6518b283f7
AulaNafisah <auladempet@gmail.com>
2024-01-15 15:05:35 +0700

    Initial commit

---
+++ app/src/main/java/com/example/jobshet12_aula/aula_activity12.kt
@@ -0,0 +1,11 @@
+package com.example.jobshet12_aula
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class aula_activity12 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aula12)
+    }
+}
